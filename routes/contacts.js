704242d33cc75e1b3374a9a6cd8e76df46d4f56b
wafflewitch <jessicapruitt717@gmail.com>
2020-05-21 17:00:44 -0500

    Define routes in server.js. Create routes directory and files. Set up routes with stub messages.

---
+++ routes/contacts.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+// @route       GET api/contacts
+// @desc        Get all contacts belonging to user
+// @access      Private
+router.get('/', (req, res) => {
+  res.send('Get all contacts');
+});
+
+// @route       POST api/contacts
+// @desc        Create a new contact
+// @access      Private
+router.post('/', (req, res) => {
+  res.send('Create a new contact');
+});
+
+// @route       PUT api/contacts/:id
+// @desc        Update a contact
+// @access      Private
+router.put('/:id', (req, res) => {
+  res.send('Update a contact');
+});
+
+// @route       DELETE api/contacts/:id
+// @desc        Delete a contact
+// @access      Private
+router.delete('/:id', (req, res) => {
+  res.send('Delete a contact');
+});
+
+module.exports = router;
