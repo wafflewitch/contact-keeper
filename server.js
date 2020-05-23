a98ffd05d7656f9d4c8ab90b48a9553a26a4dc2e
wafflewitch <jessicapruitt717@gmail.com>
2020-05-22 22:34:54 -0500

    Create models directory and User.js file. Define UserSchema in User.js. Initialize middleware in server.js. Refactor create route in users.js to use validations and return errors.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect database
 connectDB();
 
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({ message: 'Hello world!' }));
 
 // Define routes
