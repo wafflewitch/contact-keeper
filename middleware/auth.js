10a45a2826e887faa5a46c73e05436650c5aea41
wafflewitch <jessicapruitt717@gmail.com>
2020-05-26 20:37:10 -0500

    Create middleware directory and auth.js file. In middleware/auth.js, get token from request header and verify the user or return an error. In routes/auth.js, refactor get request to return logged-in user by using the result from middleware/auth.js.

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+
+  // Check if the token is missing
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: 'Authorization denied due to missing token.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Invalid token.' });
+  }
+};
