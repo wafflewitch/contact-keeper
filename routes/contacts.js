a01137c046353bb86b484bd2b315c3fd0632b558
wafflewitch <jessicapruitt717@gmail.com>
2020-05-29 18:57:18 -0500

    Refactor routes/contacts.js so that put request updates an existing contact or creates a new one.

--- routes/contacts.js
+++ routes/contacts.js
@@ -58,8 +58,37 @@ router.post(
 // @route       PUT api/contacts/:id
 // @desc        Update a contact
 // @access      Private
-router.put('/:id', (req, res) => {
-  res.send('Update a contact');
+router.put('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build Contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+
+    // Makre sure User owns Contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true } // This creates a new contact if a match is not found
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route       DELETE api/contacts/:id
