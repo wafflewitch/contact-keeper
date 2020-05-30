5b1f5c104589e83928a3629713e8e912ef817a91
wafflewitch <jessicapruitt717@gmail.com>
2020-05-29 19:29:55 -0500

    Refactor routes/contacts.js so that delete request removes a given contact.

--- routes/contacts.js
+++ routes/contacts.js
@@ -95,8 +95,24 @@ router.put('/:id', auth, async (req, res) => {
 // @route       DELETE api/contacts/:id
 // @desc        Delete a contact
 // @access      Private
-router.delete('/:id', (req, res) => {
-  res.send('Delete a contact');
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+
+    // Makre sure User owns Contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Contact removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
