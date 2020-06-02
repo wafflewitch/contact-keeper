e72b3927d51cabffc8016384d018ef6164e5bcc6
wafflewitch <jessicapruitt717@gmail.com>
2020-06-01 23:23:39 -0500

    Create Navbar component and import into App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 const App = () => {
-  return <div className='App'>Contact Keeper</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+      Contact Keeper
+    </div>
+  );
 };
 
 export default App;
