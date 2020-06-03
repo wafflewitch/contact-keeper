c57a68cfd0360e2036296ae998d6fd2115f094ab
wafflewitch <jessicapruitt717@gmail.com>
2020-06-02 20:55:10 -0500

    Add home and about links to navbar using react-router-dom. Create pages directory and Home and About components within.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
   return (
     <div className='navbar bg-primary'>
       <h1>
-        <i className={icon} />
-        {title}
+        <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Contact Keeper</h1>
+    </div>
+  );
+};
+
+export default Home;
