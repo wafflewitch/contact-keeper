f82d8fdac533c229fd3e2aa2f265d133ed4422e3
wafflewitch <jessicapruitt717@gmail.com>
2020-06-06 23:29:25 -0500

    Set up contactReducer and switch case for ADD_CONTACT.

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,23 @@
+import {
+  ADD_CONTACT,
+  UPDATE_CONTACT,
+  DELETE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SET_ALERT,
+  REMOVE_ALERT,
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    default:
+      return state;
+  }
+};
