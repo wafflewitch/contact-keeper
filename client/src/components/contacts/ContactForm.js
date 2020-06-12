b0ef9e335f951008585c96928f80308a4461c0ea
wafflewitch <jessicapruitt717@gmail.com>
2020-06-11 22:08:55 -0500

    Make title dynamic between 'add contact' and 'edit contact' in ContactForm component.

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -44,7 +44,9 @@ const ContactForm = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <h2 className='text-primary'>Add New Contact</h2>
+      <h2 className='text-primary'>
+        {current ? 'Edit Contact' : 'Add Contact'}
+      </h2>
       <input
         type='text'
         placeholder='Name'
