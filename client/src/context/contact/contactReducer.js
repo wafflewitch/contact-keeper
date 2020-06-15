af5fdd6653acfc23de5d1c87068c73fd20eb1e93
wafflewitch <jessicapruitt717@gmail.com>
2020-06-14 22:59:16 -0500

    Add a case for UPDATE_CONTACT in the reducer.

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -17,6 +17,15 @@ export default (state, action) => {
         ...state,
         contacts: [...state.contacts, action.payload],
       };
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: [
+          ...state.contacts.map((contact) =>
+            contact.id === action.payload.id ? action.payload : contact
+          ),
+        ],
+      };
     case DELETE_CONTACT:
       return {
         ...state,
