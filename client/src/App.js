7d27cee913211e798b5f1b336c36f871cde31352
wafflewitch <jessicapruitt717@gmail.com>
2020-06-24 22:33:36 -0500

    Import setAuthToken into App.js and add conditional that invokes it if localStorage has a token.

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,12 @@ import Alerts from './components/layout/Alerts';
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
