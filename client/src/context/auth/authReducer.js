d5dc401b0c634e7ba66b7aa2b52254b62f13e03d
wafflewitch <jessicapruitt717@gmail.com>
2020-06-25 18:29:49 -0500

    In authReducer, add switch cases for LOGIN_SUCCESS and LOGIN_FAIL.

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,6 +19,7 @@ export default (state, action) => {
         user: action.payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
