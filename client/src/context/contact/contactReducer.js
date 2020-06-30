9c0c1ff1d253e6e7c17f96b46cc81c3910b23f5f
wafflewitch <jessicapruitt717@gmail.com>
2020-06-29 22:03:48 -0500

    In contactReducer, add switch case for GET_CONTACTS. Also, set loading to false for CRUD actions.

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -9,23 +9,30 @@ import {
   CLEAR_CURRENT,
   SET_ALERT,
   REMOVE_ALERT,
+  GET_CONTACTS,
 } from '../types';
 
 export default (state, action) => {
   switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+        loading: false,
       };
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: [
-          ...state.contacts.map((contact) =>
-            contact._id === action.payload.id ? action.payload : contact
-          ),
-        ],
+        contacts: state.contacts.map((contact) =>
+          contact._id === action.payload.id ? action.payload : contact
+        ),
+        loading: false,
       };
     case DELETE_CONTACT:
       return {
@@ -33,6 +40,7 @@ export default (state, action) => {
         contacts: state.contacts.filter(
           (contact) => contact._id !== action.payload
         ),
+        loading: false,
       };
     case CONTACT_ERROR:
       return {
